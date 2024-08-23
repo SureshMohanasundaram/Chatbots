11cffcd129c1fa3ad372301452a05678b2038cc2
Suresh Mohanasundaram <68373209+SureshMohanasundaram@users.noreply.github.com>
2024-08-23 16:25:02 +0530

    app.py

--- app.py
+++ app.py
@@ -5,11 +5,43 @@ st.set_page_config(page_title="Enhanced PDFChat",layout="wide",initial_sidebar_s
 
 async def main():
     st.title("PDFChat: ")
-<<<<<<< HEAD
-    option=st.selectbox("Select Option",("PDF","Blogs","Database"))
-=======
+    
     option=st.selectbox("Select Option",("PDF","Database"))
->>>>>>> c29084e96579a8ab15e8a7a1f849e10359650224
+    
+    if option == "PDF":
+        uploaded_file = st.file_uploader("Choose a file",type="PDF")
+        if uploaded_file is not None:
+            with st.spinner("Processing..."):
+                uploaded_file.seek(0)
+                file = uploaded_file.read()
+            
+            st.session_state["ready"] = True
+    
+    
+    elif option == "Database":
+        uploaded_file = st.file_uploader("Choose a Database file",type="db")
+        if uploaded_file is not None:
+            with st.spinner("Processing..."):
+                uploaded_file.seek(0)
+                file = uploaded_file.read()
+            
+            st.session_state["ready"] = True
+    
+    if st.session_state.get("ready",False):
+        if 'generated' not in st.session_state:
+            st.session_state['generated'] = ["Welcome! you can ask any questions"]
+            
+        if 'past' not in st.session_state:
+            st.session_state['past'] = ["Hey"]
+            
+        container = st.container()
+        response_container = st.container()
+        
+        with container:
+            with st.form(key='my_form',clear_on_submit=True):
+                user_input = st.text_input("Query:",placeholder="e.g.Summarize the document",key='input')
+                submit_button = st.form_submit_button(label="Send")
+
     
 if __name__=="__main__":
     asyncio.run(main())
